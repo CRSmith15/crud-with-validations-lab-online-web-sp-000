9c6bde5d86dd5a72b3996108d8a8800acbbf0eb3
Chance Smith <chancesmith97@hotmail.com>
2020-08-05 23:26:27 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,5 @@
 class Song < ApplicationRecord
   validates :title, :artist_name, presence: true
   validates :artist_name, uniqueness: { scope: :year}
-  validates :release_year, numericality: { less_than_or_equal_to: DateTime.current.to_date }
+  validates :release_year, numericality: { less_than_or_equal_to: 2020 }
 end
